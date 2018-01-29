a1cab5ebffaa883c5627f6bfb0df3d78d57131b0
yoosw00 <yoosw00@gmail.com>
2018-01-29 19:54:42 +0900

    create test code

---
+++ nvr_uv-test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cy7c65211 import *
+from nvr_uv import *
+from IPython import embed
+
+# dll = "c:/app/Cypress/Cypress-USB-Serial/library/lib/cyusbserial.dll"
+# dll = "C:\\Program Files (x86)\\Cypress\\USB-Serial SDK\\library\\cyusbserial\\x64\\cyusbserial.dll"
+dll = "/usr/local/lib/libcyusbserial.so"
+lib = CyUSBSerial(lib=dll)
+dev = lib.find().next()
+ffi = lib.ffi
+
+i2c = CyI2C(dev)
+i2c.debug = 1
+
+mpu = NVR_UV(i2c)
+
+embed()
